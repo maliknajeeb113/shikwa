cd4cf5089be29bea61f5e58455e548e912755e96
maliknajeeb133 <maliknajeeb113@gmail.com>
2022-01-02 22:40:54 +0530

    createapp error

--- shikwa/routes.py
+++ shikwa/routes.py
@@ -3,7 +3,8 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from shikwa import app, db, bcrypt, mail
-from shikwa.forms import (RegistrationForm, LoginForm, UpdateAccountForm,PostForm, RequestResetForm, ResetPasswordForm)
+from shikwa.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
+                             PostForm, RequestResetForm, ResetPasswordForm)
 from shikwa.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 from flask_mail import Message
