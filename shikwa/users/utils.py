e901ed51f15461df1544c843ae7c099f041dae57
maliknajeeb133 <maliknajeeb113@gmail.com>
2022-01-23 04:29:41 +0530

    final ready for review, no message option or like button yet

--- shikwa/users/utils.py
+++ shikwa/users/utils.py
@@ -25,7 +25,7 @@ def send_reset_email(user):
     msg = Message('Password Reset Request',
                   sender='noreply@demo.com',
                   recipients=[user.email])
-    msg.body = f'''To reset your password, visit the following link:
+    msg.body = f'''Dear User, This is your link to reset your password.
 {url_for('users.reset_token', token=token, _external=True)}
 
 If you did not make this request then simply ignore this email and no changes will be made.
