7fa8bec27581043a04c6c7355f5b521494f4cfeb
Malik Najeeb Ul Habib <60484872+maliknajeeb113@users.noreply.github.com>
2023-07-16 20:48:09 +0530

    Update config.py
    trying vercel

--- shikwa/config.py
+++ shikwa/config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config:
-    SECRET_KEY = '-----' #enter a secret key
+    SECRET_KEY = '13032001'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = '-----' #enter the mail here
-    MAIL_PASSWORD = '-----' #enter the password
+    MAIL_USERNAME = 'app.shikwa@gmail.com'
+    MAIL_PASSWORD = 'kmdxbthmsftwlawl'
